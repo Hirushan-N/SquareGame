265dcd72f1ef88b63d623a560b8fd872724e1897
Hirushan-N <nadeeshhirushan@gmail.com>
2025-01-11 10:15:13 +0530

    Initial Commit

---
+++ SquareGame/SquareGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquareGameApp.swift
+//  SquareGame
+//
+//  Created by Nadeesh Hirushan on 2025-01-11.
+//
+
+import SwiftUI
+
+@main
+struct SquareGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SquareGameTests/SquareGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  SquareGameTests.swift
+//  SquareGameTests
+//
+//  Created by Nadeesh Hirushan on 2025-01-11.
+//
+
+import Testing
+@testable import SquareGame
+
+struct SquareGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
